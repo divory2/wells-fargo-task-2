3575bf0d026b827517d17c2b3666c3efa9550909
Devon  Ivory <devonivory@Devons-MacBook-Air.local>
2026-03-09 14:40:28 -0400

    added logic for loading animation for css and added axios command to send request to api to validate login

--- wells-fargo-dashboard/src/App.js
+++ wells-fargo-dashboard/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-import {useRef} from "react";
+import {useRef, useState} from "react";
+import axios from "axios";
 function App() {
   const inputRef = useRef(null);
   const emailRef = useRef(null);
-  const submit =() =>{
+  const [loading,setLoading] = useState(false);
+  const submit =async () =>{
     
     const emailInput = emailRef.current;
     const passwordInput = inputRef.current;
@@ -11,19 +13,32 @@ function App() {
   if (!emailInput.checkValidity() || !passwordInput.checkValidity()) { // ✅ catches invalid email
     alert(emailInput.validationMessage +" " + passwordInput.validationMessage); // shows built-in message
     return;
-  }
-  
- 
-   
-    
+  } 
+  setLoading(true)
+    try{
+      const response = await axios.post("/api/login", 
+        { emailInput, 
+          passwordInput 
 
-   
-    
+        });
+      console.log("Login successful:", response.data);
+    }
+    catch(error)
+    {
+      console.error("Login failed:", error);
+      alert("Login failed. Please try again.");
+
+    }
+    finally{
+      setLoading(false);
+    }
 }
   return (
     
     <>
+    
     <header>
+
       <div id="title">
         {/* <img src='wells-fargo-dashboard/public/wells-Fargo.png' alt='logo'> </img> */}
         <img src='/wells-Fargo.png' alt='wellsFargo'></img>
@@ -32,14 +47,14 @@ function App() {
 
     </header>
     <main id='form-body'>
-      
-      <label>email</label>
-      <input id = "email" required ref={emailRef} type ='email' ></input>
-      <label>Password</label>
-      <input required type='text' ref={inputRef}></input>
-      <button id = "submit" onClick={submit}>Login</button>
-
-
+      {loading ? (
+      <div className='loading-container'>
+        <div className='spinner'></div>
+      </div>
+    ) :(
+      <><label>email</label><input id="email" required ref={emailRef} type='email'></input><label>Password</label><input required type='text' ref={inputRef}></input><button id="submit" onClick={submit}>Login</button></>
+    )}
+  
     </main>
 
    </>
