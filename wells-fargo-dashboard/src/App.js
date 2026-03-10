bdd92ae7a916a975a926c214b40a6200d727a58e
Devon  Ivory <devonivory@Mac.lan>
2026-03-10 13:08:43 -0400

    added test for checking password and email field valadation and clicking submit button the last test is to check successfull login

--- wells-fargo-dashboard/src/App.js
+++ wells-fargo-dashboard/src/App.js
@@ -11,7 +11,7 @@ function App() {
     const passwordInput = inputRef.current;
 
   if (!emailInput.checkValidity() || !passwordInput.checkValidity()) { // ✅ catches invalid email
-    alert(emailInput.validationMessage? emailInput.validationMessage + " email":""+" " + passwordInput.validationMessage ? passwordInput.validationMessage + "password":""); // shows built-in message
+    alert((emailInput.validationMessage? emailInput.validationMessage + ": email":"") + " " + (passwordInput.validationMessage ? passwordInput.validationMessage + ": password":"")); // shows built-in message
     return;
   } 
   setLoading(true)
@@ -49,11 +49,11 @@ function App() {
     </header>
     <main id='form-body'>
       {loading ? (
-      <div className='loading-container'>
-        <div className='spinner'></div>
+      <div className='loading-container' id='loding'>
+        <div className='spinner' id='spinner'></div>
       </div>
     ) :(
-      <><label htmlFor="email">email</label><input id="email"  data-testid="email" required ref={emailRef} type='email'></input><label htmlFor='password'>Password</label><input required type='text' ref={inputRef} id='password' data-testid="password"></input><button id="submit" onClick={submit}>Login</button></>
+      <><label htmlFor="email">email</label><input id="email"  data-testid="email" required ref={emailRef} type='email'></input><label htmlFor='password'>Password</label><input required type='text' ref={inputRef} id='password' data-testid="password"></input><button id="login" onClick={submit}>Login</button></>
     )}
   
     </main>
--- wells-fargo-dashboard/src/App.test.js
+++ wells-fargo-dashboard/src/App.test.js
@@ -1,6 +1,7 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 import userEvent from '@testing-library/user-event'
+import axios from 'axios';
 // import axios from 'axios';
 jest.mock('axios');
 describe('Testing the login screen', () => {
@@ -34,6 +35,89 @@ test('check password field valadation', async () => {
    await userEvent.type(password, userPassword);
    expect(password.value).toBe(userPassword);
 });
-}) ;
+test('When submit button is pressed and fields both fields are empty', async () => {
+  window.alert = jest.fn();
+  
+  const loginButton = await screen.findByRole('button', {name: /login/i });
+   await userEvent.click(loginButton);
+  expect(window.alert).toHaveBeenCalled();
+
+   
+});
+test('When submit button is pressed and password fields has value and email field is empty', async () => {
+  window.alert = jest.fn();
+  
+  const loginButton = await screen.findByRole('button', {name: /login/i });
+  const userPassword = "dsdfws";
+  const password = await screen.findByRole('textbox', {name: /password/i });
+   await userEvent.type(password, userPassword);
+   await userEvent.click(loginButton);
+  expect(window.alert).toHaveBeenCalledWith("Constraints not satisfied: email ")
+  expect(window.alert).toHaveBeenCalledTimes(1);
+  alert.mockRestore();
+
+  // Please fill out this field.: email Please fill out this field.: password
+});
+test('When submit button is pressed and password fields empty and email field is has proper email format', async () => {
+  window.alert = jest.fn();
+  
+  const loginButton = await screen.findByRole('button', {name: /login/i });
+  const userEmail = "devonivory27@gmail.com";
+  const email = await screen.findByRole('textbox', {name: /email/i });
+   await userEvent.type(email,userEmail);
+   await userEvent.click(loginButton);
+  expect(window.alert).toHaveBeenCalledWith(" Constraints not satisfied: password");
+  expect(window.alert).toHaveBeenCalledTimes(1);
+  alert.mockRestore();
+
+  // Please fill out this field.: email Please fill out this field.: password
+});
+test('When submit button is pressed and password fields has a value  and email field does not have proper email format', async () => {
+  window.alert = jest.fn();
+  
+  const loginButton = await screen.findByRole('button', {name: /login/i });
+  const userEmail = "devonivory27gmail.com";
+  const userPassword = "dsdfws";
+  const password = await screen.findByRole('textbox', {name: /password/i });
+   await userEvent.type(password, userPassword);
+  const email = await screen.findByRole('textbox', {name: /email/i });
+   await userEvent.type(email,userEmail);
+   await userEvent.click(loginButton);
+  expect(window.alert).toHaveBeenCalledWith("Constraints not satisfied: email ");
+  expect(window.alert).toHaveBeenCalledTimes(1);
+  alert.mockRestore();
+
+  // Please fill out this field.: email Please fill out this field.: password
+});
+test('When submit button is pressed and password fields has a value  and email field does not have proper email format', async () => {
+  const alertMock = jest.spyOn(window, "alert").mockImplementation(() => {});
+
+  window.alert = jest.fn();
+  
+  const loginButton = await screen.findByRole('button', {name: /login/i });
+  const userEmail = "devonivory27@gmail.com";
+  const userPassword = "dsdfws";
+  const password = await screen.findByRole('textbox', {name: /password/i });
+  const email = await screen.findByRole('textbox', {name: /email/i });
+  await userEvent.type(password, userPassword);
+ 
+   await userEvent.type(email,userEmail);
+   await userEvent.click(loginButton);
+   const mockData = { data: { message: "Login Successfully" } };
+   axios.post.mockResolvedValue(mockData);
+  
+
+  expect(window.alert).toHaveBeenCalledTimes(1);
+  
+  alert.mockRestore();
+  await waitFor (( () => {
+     expect(alertMock).toHaveBeenCalledWith("Login Successfully");
+  }));
+ 
+
+  // Please fill out this field.: email Please fill out this field.: password
+});
+
+}) 
 
 
