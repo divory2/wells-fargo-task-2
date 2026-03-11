200226fac992e5222c3ed369f257f775600d729c
Devon  Ivory <devonivory@Mac.lan>
2026-03-11 17:30:00 -0400

    created test for advisorcontroller and entrypoint test

---
+++ src/test/java/com/example/demo/AdvisorControllerTest.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(MyController.class)
+public class AdvisorControllerTest {
+
+}
---
+++ src/test/java/com/example/demo/EntrypointTest.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.wellsfargo.counselor.Entrypoint;
+import com.wellsfargo.counselor.Controller.AdvisorController;
+import static org.assertj.core.api.Assertions.assertThat;
+@SpringBootTest(classes= Entrypoint.class)// means spring boot will look for configuration
+//class with springboot application test 
+ class EntrypointTest {
+    @Autowired
+    private AdvisorController controller;
+    @Test
+    void contextLoads(){
+        assertThat(controller).isNotNull();
+    }
+
+}
