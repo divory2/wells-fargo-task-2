d8cb0c38d47738a36917c6ea177a550bca2bee14
Devon  Ivory <devonivory@Mac.lan>
2026-03-11 17:33:37 -0400

    corrected mycontroller class and put advisorController class

--- src/test/java/com/example/demo/AdvisorControllerTest.java
+++ src/test/java/com/example/demo/AdvisorControllerTest.java
@@ -2,7 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
-@WebMvcTest(MyController.class)
+import com.wellsfargo.counselor.Controller.AdvisorController;
+
+@WebMvcTest(AdvisorController.class)
 public class AdvisorControllerTest {
 
 }
