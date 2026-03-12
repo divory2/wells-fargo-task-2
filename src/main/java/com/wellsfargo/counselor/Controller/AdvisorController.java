5837de832d046574b92f0bedb58139511e665351
Devon  Ivory <devonivory@Mac.lan>
2026-03-12 12:39:06 -0400

    added get mapping to AdvisorController

--- src/main/java/com/wellsfargo/counselor/Controller/AdvisorController.java
+++ src/main/java/com/wellsfargo/counselor/Controller/AdvisorController.java
@@ -1,10 +1,18 @@
 package com.wellsfargo.counselor.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "api/v1/advisor")
 public class AdvisorController {
+
+    @GetMapping(path = "/get")
+    public Object getAdvisor() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAdvisor'");
+    }
+
 
 }
