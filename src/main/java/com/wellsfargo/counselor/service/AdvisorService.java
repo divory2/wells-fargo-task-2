b927913c3e1cbb4539d0022fbcb478ef2406253b
Devon  Ivory <devonivory@Mac.lan>
2026-03-12 12:41:56 -0400

    created Advisorservice and created dummy method

---
+++ src/main/java/com/wellsfargo/counselor/service/AdvisorService.java
@@ -0,0 +1,14 @@
+package com.wellsfargo.counselor.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdvisorService {
+
+    public Object getAdvisor() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAdvisor'");
+    }
+
+
+}
